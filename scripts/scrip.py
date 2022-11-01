b9f6e0ca7a5a771289b04ba16999d9284c94e82f
Cyber Range Student <student@ip-10-1-49-205.ec2.internal>
2022-11-01 01:02:52 +0000

    ThreadShed.py

---
+++ scripts/scrip.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+order = ["daisy", "buttercup", "snapdragon", "gardenia", "lily"]
+
+new_order = ["lilac", "iris"]
+
+orders_combined = order + new_order
+
+print = orders_combined
+
+
+
--- scripts/scripts1/vulnscan.py
+++ scripts/scripts1/vulnscan.py
@@ -5,7 +5,6 @@ from IPy import IP
 import pyfiglet
 import subprocess
 import time
-from datetime import datetime
 
 #//clear the terminal window//
 subprocess.call('clear', shell=True)
@@ -26,7 +25,7 @@ def scan(target):
 	'''//Scan target funtion//'''
 	converted_ip = convert_ip(target)
 	print('\n' + 'Scanning Target : ' + str(target))
-	for port in range(int(start_port),int(end_port)):
+	for port in range(21,80):
 		scan_port(converted_ip, port)
 
 def convert_ip(ip):
@@ -48,18 +47,6 @@ def scan_port(ipaddress, port):
 		sock.settimeout(.01)
 		sock.connect((ipaddress, port))
 		try:
-			banner = get_banner(sock)
-			print('[+] Port ' + str(port) + ' is Open ' + ':' + str(banner.decode().strip('\n')))
-		except:
-			print('[+] Port ' + str(port) + ' is Open ')
-	except:
-		pass
-	try:
-		sock = socket.socket()
-		sock.settimeout(.01)
-		sock.connect((ipaddress, port))
-
-		try:
 			ports.append(port)
 			banner = sock.recv(1024).decode().strip('\n').strip('\r')
 			banners.append(banner)
@@ -70,16 +57,7 @@ def scan_port(ipaddress, port):
 	except:
 		pass
 if __name__ == "__main__":
-	targets = input('[+] Enter Target/s To Scan(for multiple targets use a coma): ' )
-	start_port = input('enter the port to start the scan: ')
-	end_port = input('enter the port to end the scan: ')
-	#start_port = int(start_port)
-	#end_port = int(end_port)
-	#check start time
-	time_start = datetime.now().replace(microsecond=0)
-	print("Scanning started at: " + str(time_start))
-
-
+	targets = input( '[+] Enter Target/s To Scan(for multiple targets use a coma): ' )
 	if ',' in targets:
 		for ip_address in targets.split(','):
 			scan(ip_address.strip(' '))
@@ -97,13 +75,4 @@ with open("vulnerable_banners.txt",'r') as file:
 			if line.strip() in banner:
 				print('[!!] VULNERABLE BANNER: "' + banner + '" ON PORT: ' + str(ports[count]))
 	count += 1
-#check completed time 
-time_completed = datetime.now().replace(microsecond=0)
-
-#Total time the Scan look
-time_total = time_completed - time_start
-
-print("Scanning ended at:" + str(time_completed))
-print("Scanning Completed in " + str(time_total))
-
 
